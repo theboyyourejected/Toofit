fc0eda4cd75b62dad62721c53572765ba9c455c5
Olaide Daniel Oluwatosin <160068457+theboyyourejected@users.noreply.github.com>
2024-06-17 14:43:58 -0700

    Add files via upload

---
+++ Project too fit (coming soon) - Copy/script.js
@@ -0,0 +1,6 @@
+function showAm() {
+    document.getElementById("myfirstnav").style.display="inline-block"
+}
+function commotAm() {
+    document.getElementById("myfirstnav").style.display="none"
+}
